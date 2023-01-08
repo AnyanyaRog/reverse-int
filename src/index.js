ff457efd033648043d16bc43a6fcbe17ac754593
AnyanyaRog <anyaroganova2002@yandex.ru>
2023-01-08 15:32:45 +0300

    docks: java script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+  let array = ""
+  if (n < 0) {
+    n = n*(-1)
+ }
+ let str = String(n)
+  for (let i=str.length-1; i>=0; i--)
+ array += str[i];
+ return array;
+} 
